747add2c59daa5db1373dcb7d67084869d367a86
Azlan <mohdazlan.abas@gamil.com>
2025-12-30 11:43:26 +0800

    Committed From Zsh At 11:43 a.m., Tuesday, December 30, 2025

--- fib.js
+++ fib.js
@@ -1,22 +1,20 @@
 function fibonacci(n) {
-    if (n <= 1) return n;
-    let a = 0n, b = 1n, temp;
-    for (let i = 2; i <= n; i++) {
+    if (n <= 1)
+        return BigInt(n);
+    var a = 0n, b = 1n, temp;
+    for (var i = 2; i <= n; i++) {
         temp = a + b;
         a = b;
         b = temp;
     }
     return b;
 }
-
-const startTime = process.hrtime.bigint();
-const result = fibonacci(100);
-const endTime = process.hrtime.bigint();
-
-const timeSeconds = Number(endTime - startTime) / 1e9;
-const memoryUsed = Math.round(process.memoryUsage().heapUsed / 1024);
-
-console.log("Language: JavaScript");
-console.log(`Time: ${timeSeconds.toFixed(6)} seconds`);
-console.log(`Memory: ${memoryUsed} KB`);
-console.log(`Result: ${result}`);
+var startTime = process.hrtime.bigint();
+var result = fibonacci(100);
+var endTime = process.hrtime.bigint();
+var timeSeconds = Number(endTime - startTime) / 1e9;
+var memoryUsed = Math.round(process.memoryUsage().heapUsed / 1024);
+console.log("Language: TypeScript");
+console.log("Time: ".concat(timeSeconds.toFixed(6), " seconds"));
+console.log("Memory: ".concat(memoryUsed, " KB"));
+console.log("Result: ".concat(result));
